0ff346c7c03cc4bd8f22f4dab1cc79b3d2fc00d2
Kayle Maximus <kaylemaximus@gmail.com>
2023-08-10 11:13:54 +0700

    Screen Wrap Around (using Viewport)

--- Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,7 +22,48 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if( Touchscreen.current.primaryTouch.press.isPressed){
+        ProcessInput();
+        KeepPlayerOnScreen();
+    }
+
+    private void KeepPlayerOnScreen()
+    {
+        Vector3 newPosition = transform.position;
+        Vector3 viewportPosition = _mainCamera.WorldToViewportPoint(transform.position);
+
+        if (viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if (viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+        if (viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if (viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_movementDirection == Vector3.zero) { return; }
+
+        _rb.AddForce(_movementDirection * _forceMagnitude * Time.deltaTime, ForceMode.Force);
+
+        _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxVelocity);
+    }
+
+    private void ProcessInput()
+    {
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
             Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
@@ -29,16 +71,10 @@ public class PlayerMovement : MonoBehaviour
             _movementDirection = transform.position - worldPosition;
             _movementDirection.z = 0f;
             _movementDirection.Normalize();
-        }else{
+        }
+        else
+        {
             _movementDirection = Vector3.zero;
         }
-    }
-
-    private void FixedUpdate(){
-        if(_movementDirection == Vector3.zero){return;}
-
-        _rb.AddForce(_movementDirection * _forceMagnitude * Time.deltaTime, ForceMode.Force);
-
-        _rb.velocity = Vector3.ClampMagnitude(_rb.velocity,_maxVelocity);
     }
 }
